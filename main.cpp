733e1e0a9b17d5843060e6f3349cd2df2e47e322
Jackson Kidwell <130793828+Wardence1@users.noreply.github.com>
2025-01-15 21:37:15 -0500

    Movement and turning

--- main.cpp
+++ main.cpp
@@ -2,30 +2,14 @@
 #include <eigen3/Eigen/Core>
 #include <SDL2/SDL.h>
 
-#define MAP_WIDTH 12
-#define MAP_HEIGHT 12
-#define TILE_SIZE 64
-#define WIN_WIDTH TILE_SIZE*MAP_WIDTH
-#define WIN_HEIGHT TILE_SIZE*MAP_HEIGHT
+#define WIN_WIDTH 1080
+#define WIN_HEIGHT 720
 
 #define FPS 60
 
-int map[MAP_HEIGHT][MAP_WIDTH] = {
-	{1,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,1,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,1,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,1,0,0,0,0,0,0,0},
-};
 
 Eigen::Vector2d rotate(Eigen::Vector2d vec, double rads);
+
 
 int main() {
 
@@ -60,18 +44,10 @@ int main() {
 	Eigen::Vector2d pos(50, 50);
 	Eigen::Vector2d dir(1, 0);
 	Eigen::Vector2d plane(0, .6);
-	double turnSp = .2;
-	double lineLen = 26;
-
 	while (running) {
 		while (SDL_PollEvent(&e)) {
-			switch(e.type) {
-				case SDL_QUIT:
-					running = false;
-					break;
-				case SDLK_LEFT: // @todo: input and turning
-					rotate(pos, turnSp);
-					break;
+			if (e.type == SDL_QUIT) {
+				running = false;
 			}
 		}
 
@@ -86,8 +62,6 @@ int main() {
 		if (acc >= 1000 / FPS) {
 			acc = 0;
 			ticks++;
-
-				
 		}
 
 			
@@ -96,40 +70,8 @@ int main() {
 		//////////
 		SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 		SDL_RenderClear(ren);
-
-		SDL_Rect rect; // multi-purpose rect
-		// Grid
-		for (int x = 0; x < MAP_WIDTH; x++) {
-			for (int y = 0; y < MAP_HEIGHT; y++) {
-				rect.x = x * TILE_SIZE;
-				rect.y = y * TILE_SIZE;
-				rect.w = TILE_SIZE - 2;
-				rect.h = TILE_SIZE - 2;
-		
-				if (map[y][x] == 1) {
-					SDL_SetRenderDrawColor(ren, 0, 0, 122, 255);
-					SDL_RenderFillRect(ren, &rect);
-				} else {
-					SDL_SetRenderDrawColor(ren, 22, 22, 22, 255);
-					SDL_RenderFillRect(ren, &rect);
-				}
-			}
-		}
-
-		// Player		
-		rect.w = 8;
-		rect.h = 8;
-		rect.x = pos.x()-rect.w/2;
-		rect.y = pos.y()-rect.h/2;
-
-		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
-		SDL_RenderFillRect(ren, &rect);
-		// Line
-		SDL_RenderDrawLine(ren, (int)pos.x(), (int)pos.y(),
-								(int)(pos.x()+dir.x()*lineLen), (int)(pos.y()+dir.y()*lineLen));
-				
-
 		SDL_RenderPresent(ren);
+
 	}
 
 	SDL_DestroyRenderer(ren);	
@@ -139,11 +81,12 @@ int main() {
 	return 0;
 }
 
+// @test: make sure this works
 Eigen::Vector2d rotate(Eigen::Vector2d vec, double rads) {
 	
 	Eigen::Matrix2d rotMat;
-	rotMat << std::cos(rads), -std::sin(rads),
-	       std::sin(rads), std::cos(rads);
-	
+	rotMat << std::cos(rads), std::sin(rads),
+	       -std::sin(rads), std::cos(rads);
+
 	return rotMat * vec; 
 }
